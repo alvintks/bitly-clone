e71f642fbcf596a899dcd4d6c12bde57cda62802
alvintks <alvintankuansean@gmail.com>
2017-06-22 17:42:10 +0800

    fix malfunction button

--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   $('form').on('submit', function(event) { 
-  event.preventDefault() 
+    event.preventDefault() 
     $.ajax({ 
     url: '/add_url',
     method: 'post', 
     data: $(this).serialize(),
 
-      success: function(data) {
+    success: function(data) {
       result = JSON.parse(data) 
       $('tr:first-child').after('<tr><td>' + result.long_url + 
         '</td><td><a href="' + result.short_url + '">'+ result.short_url +'</a></td><td>' 
